46c60c598fe5b36c4c03a18047eba99b53ec3af9
MeowMeowWithWind <chenlaoer82@hotmail.com>
2023-02-06 04:46:38 +0800

    Add phoneme mode for inference and add a denoise option

--- inference.py
+++ inference.py
@@ -11,7 +11,7 @@ import soundfile as sf
 import commons
 import utils
 from models import SynthesizerTrn
-from text import create_symbols_manager, text_to_sequence
+from text import create_symbols_manager, text_to_sequence, cleaned_text_to_sequence, _clean_text
 
 import argparse
 
@@ -20,7 +20,10 @@ class AudioGenerator():
         self.hparams = hparams
         self._device = device
 
-        symbols_manager = create_symbols_manager(hparams.data.language)
+        if 'language' in hps.data:
+            symbols_manager = create_symbols_manager(hparams.data.language)
+        else:
+            symbols_manager = create_symbols_manager('default')
         self.symbol_to_id = symbols_manager._symbol_to_id
 
         self.net_g = create_network(hparams, symbols_manager.symbols, device)
@@ -28,11 +31,17 @@ class AudioGenerator():
     def load(self, path):
         load_checkpoint(self.net_g, path)
 
-    def inference(self, text):
-        return do_inference(self.net_g, self.hparams, self.symbol_to_id, text, self._device)
+    def inference(self, text, phoneme_mode=False):
+        return do_inference(self.net_g, self.hparams, self.symbol_to_id, text, phoneme_mode, self._device)
 
-def get_text(text, hparams, symbol_to_id):
-    text_norm = text_to_sequence(text, hparams.data.text_cleaners, symbol_to_id)
+def get_text(text, hparams, symbol_to_id, phoneme_mode=False):
+    if not phoneme_mode:
+        print(_clean_text(text, hparams.data.text_cleaners))
+        text_norm = text_to_sequence(text, hparams.data.text_cleaners, symbol_to_id)
+    else:
+        print(text)
+        text_norm = cleaned_text_to_sequence(text, symbol_to_id)
+        
     if hparams.data.add_blank:
         text_norm = commons.intersperse(text_norm, 0)
     text_norm = torch.LongTensor(text_norm)
@@ -52,8 +61,8 @@ def load_checkpoint(network, path):
     _ = utils.load_checkpoint(path, network, None)
 
 # Assume the network has loaded weights and are ready to do inference
-def do_inference(generator, hparams, symbol_to_id, text, device):
-    stn_tst = get_text(text, hparams, symbol_to_id)
+def do_inference(generator, hparams, symbol_to_id, text, phoneme_mode=False, device=torch.device('cpu')):
+    stn_tst = get_text(text, hparams, symbol_to_id, phoneme_mode)
 
     with torch.no_grad():
         x_tst = stn_tst.to(device).unsqueeze(0)
@@ -99,19 +108,24 @@ if __name__ == "__main__":
         print("Use CPU")
         device = torch.device('cpu')
 
-    config_path = "./configs/ljs_windows.json"
-    # config_path = "./configs/bb_v100.json"
+    config_path = "./configs/bb_v100.json"
     # config_path = "./configs/kkr_tiny_laptop.json"
     # config_path = "./configs/inference_ce.json"
+    # config_path = "./configs/lex_base.json"
+    # config_path = "./configs/ljs_windows.json"
     hps = utils.get_hparams_from_file(config_path)
 
     audio_generator = AudioGenerator(hps, device)
 
-    # checkpoint_path = "./models/G_lex_orig_laptop_5000.pth"
-    # checkpoint_path = "./models/G_bb_v100_820000.pth"
+    # checkpoint_path = "./models/G_lex_base_120000.pth"
+    checkpoint_path = "./models/G_bb_v100_820000.pth"
     # checkpoint_path = "./models/G_kkr_tiny_laptop_7000.pth"
-    checkpoint_path = "./models/G_ljs_windows_783000.pth"
+    # checkpoint_path = "./models/G_ljs_windows_2450000.pth"
+    # checkpoint_path = "./models/G_ruby_v100_419000.pth"
     audio_generator.load(checkpoint_path)
+
+    phoneme_mode = False
+    do_noise_reduction = True
 
     if args.filepath is not None:
         print("Batch generation:")
@@ -128,38 +142,57 @@ if __name__ == "__main__":
         start = time.perf_counter()
         index = 1
         for text in texts:
-            audio = audio_generator.inference(text)
+            audio = audio_generator.inference(text, phoneme_mode)
+
+            if do_noise_reduction:
+                import noisereduce as nr
+
+                # perform noise reduction
+                audio = nr.reduce_noise(y=audio, sr=hps.data.sampling_rate)
+
             filename = f"{args.name}_{index:04}.wav"
             output_path = os.path.join(output_dir, filename)
             save_to_wav(audio, hps.data.sampling_rate, output_path)
             index += 1
         print(f"The inference takes {time.perf_counter() - start} seconds")
     else:
-        # text = "我是御坂妹妹。"
-        # text = "我是你爹，喵喵抽风！"
+
+        text = "大家好，我是御坂美琴。"
         # text = "喵喵抽风，是乱杀之星！"
         # text = "炸鸡，是喵喵抽风的大儿子。"
-        # text = "我了解，你在人鱼港当了团长，生活的很好，有剧团和艾丽卡保护你，你不需要我这种舰娘。"
-        # text = "但是，你现在来找我说，列克星敦，我喜欢你？我到底做错了什么让你这么不尊重我？"
         # text = "卡尔普陪外孙玩滑梯。"
+        # text = "假语村言别再拥抱我。"
         # text = "他的到来是一件好事，我很欢迎他，不管是代表个人，还是代表俱乐部。"
         # text = "研究完成，dou， 您可以制定新的科研方向了，司令官。"
-        # text = "哎！司令官，就像个孩子一样调皮呢！嗯哼！不过，还挺可爱的。"
         # text = "12345！"
         
         # text = "高い山のいただきに住んで、小鳥を取って食べたり、"
 
         # text = "yi2 jian4  san1 lian2！" 
 
-        # text = "It has used other Treasury law enforcement agents on special experiments in building and route surveys in places to which the President frequently travels."
+        # text = "Printing, in the only sense with which we are at present concerned, differs from most if not from all the arts and crafts represented in the Exhibition"
 
-        text = "Printing, in the only sense with which we are at present concerned, differs from most if not from all the arts and crafts represented in the Exhibition"
+        # text = "sin nɛæn kwaɪ lɜː"
+        # text = "tool nɛæn kwaɪ lɜː"
+        # text = "tˈu nˈiːən kwˈaɪ lə:"
+       
+        # text = "e zɛæn san lɛæn"
+        # do_noise_reduction = False
         
+        # text = "Happy Chinese new year"
+        # text = "hˈæ pi tʃaɪ  nˈiːz nˈuː jˈɪɹ"
+
         start = time.perf_counter()
-        audio = audio_generator.inference(text)
+        audio = audio_generator.inference(text, phoneme_mode)
         print(f"The inference takes {time.perf_counter() - start} seconds")
 
         print(audio.dtype)
+        
+        if do_noise_reduction:
+            import noisereduce as nr
+
+            # perform noise reduction
+            audio = nr.reduce_noise(y=audio, sr=hps.data.sampling_rate)
 
         output_dir = './output/'
         # python program to check if a path exists
